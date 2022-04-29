133259e9855aee1a69fd01fef2ae9c7fed98e49c
黄新民 <huangxinmin@hzwanglan.com>
2022-04-29 17:44:17 +0800

    init

---
+++ packages/cli/lib/cli.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: 明月寒
+ * @Github: http://www.33u3.com
+ * @Date: 2022-04-29 15:18:59
+ * @LastEditors: 明月寒
+ * @LastEditTime: 2022-04-29 16:22:49
+ * @Description:
+ */
+'use strict'
+
+module.exports = cli
+
+function cli () {
+  // TODO
+  console.log('cli')
+}
